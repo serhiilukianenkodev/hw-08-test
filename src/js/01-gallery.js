bc12f744d057daa4a4007117101f3859b935f533
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-22 19:23:27 +0300

    task 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
-console.log(galleryItems);
+import { galleryItems } from './gallery-items';
+
+const galleryEl = document.querySelector('.gallery');
+
+galleryEl.innerHTML = createGallery(galleryItems);
+
+new SimpleLightbox('.gallery a', {
+  captionPosition: 'top',
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionDelay: 250,
+});
+
+function createGallery(list) {
+  return list
+    .map(
+      ({ preview, original, description }) => `
+    <li class="gallery__item">
+        <a class="gallery__link" href="${original}">
+        <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+    </li>`
+    )
+    .join('');
+}
