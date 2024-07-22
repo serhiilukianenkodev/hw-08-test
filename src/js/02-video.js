7d83c040e979f091b9c98db5985b55522d619514
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-22 20:21:39 +0300

    task 2 done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const KEY_TIME = 'current time';
+const playerEl = document.querySelector('#vimeo-player');
+
+const currentTime = getCurrentTime();
+
+const player = new Player(playerEl);
+
+player.setCurrentTime(currentTime);
+
+player.on('timeupdate', throttle(saveCurrentTime, 1000));
+
+function saveCurrentTime(time) {
+  localStorage.setItem(KEY_TIME, time.seconds);
+}
+
+function getCurrentTime() {
+  return Number(localStorage.getItem(KEY_TIME));
+}
