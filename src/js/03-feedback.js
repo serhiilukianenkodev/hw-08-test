8a10f16b93a8a9c218f424cb75a08d82ffa15068
serhiilukianenkodev <serhii.lukianenko.dev@gmail.com>
2024-07-22 21:19:03 +0300

    task 3 finished

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,42 @@
+import throttle from 'lodash.throttle';
 
+const KEY_FORM = 'form_data';
+
+const form = document.querySelector('.feedback-form');
+
+const savedData = getData();
+if (savedData) {
+  form.elements.email.value = savedData.email;
+  form.elements.message.value = savedData.message;
+}
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput() {
+  const formData = new FormData(form);
+  saveData(Object.fromEntries(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+  const email = evt.currentTarget.elements.email.value;
+  const message = evt.currentTarget.elements.message.value;
+
+  if (!(email && message)) {
+    alert('Введи всі дані!!!!!!');
+    return;
+  }
+
+  evt.currentTarget.reset();
+  localStorage.removeItem(KEY_FORM);
+  console.log({ email, message });
+}
+
+function saveData(data) {
+  localStorage.setItem(KEY_FORM, JSON.stringify(data));
+}
+
+function getData() {
+  return JSON.parse(localStorage.getItem(KEY_FORM));
+}
